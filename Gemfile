7da1ff5eb168b3086327f78be05d383d02aac90c
yongseewei <yongseewei@gmail.com>
2016-09-23 06:16:12 +0800

    added speech to text and ai api

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 gem 'simple_form', '~> 3.3', '>= 3.3.1'
 
 gem 'devise', '~> 4.2'
+
+gem "figaro"
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,139 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+var baseUrl = "https://api.api.ai/v1/",
+    $speechInput,
+    $recBtn,
+    recognition,
+    messageRecording = "Recording...",
+    messageCouldntHear = "I couldn't hear you, could you say that again?",
+    messageInternalError = "Oh no, there has been an internal server error",
+    messageSorry = "I'm sorry, I don't have the answer to that yet.";
+var accessToken = "b9d899a8ae0a4559adbc257dfe0403ec";
+var subscriptionKey = "655502d27d9f4d32b091fdbf67fe0cba";
+
+$(document).ready(function() {
+	$("#input").keypress(function(event) {
+		if (event.which == 13) {
+			event.preventDefault();
+			send();
+		}
+	});
+// $("#rec").click(function(event) {
+	if (window.speechSynthesis.speaking == false){
+		switchRecognition();
+	}
+	// });
+});
+
+		
+var temp ="";
+
+function startRecognition() {
+	recognition = new webkitSpeechRecognition();
+	recognition.continuous = true;
+	recognition.onstart = function(event) {
+		updateRec();
+	};
+	recognition.onresult = function(event) {
+		var text = "";
+	    for (var i = event.resultIndex; i < event.results.length; ++i) {
+	    	text += event.results[i][0].transcript;
+	    }
+	    setInput(text);
+		stopRecognition();
+	};
+	recognition.onend = function() {
+		// debugger
+		stopRecognition();
+	};
+	recognition.lang = "en-US";
+	recognition.start();
+}
+	
+function stopRecognition() {
+	if (recognition) {
+		recognition.stop();
+		recognition = null;
+	}
+	updateRec();
+}
+
+function switchRecognition() {
+	if (recognition) {
+		stopRecognition();
+	} else {
+		startRecognition();
+	}
+}
+
+function setInput(text) {
+	$("#input").val(text);
+	send(text);
+}
+
+function updateRec() {
+	$("#rec").text(recognition ? "Stop" : "Speak");
+}
+
+function send(text) {
+	// var text = $("#input").val();
+	$.ajax({
+		type: "POST",
+		url: baseUrl + "query/",
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		headers: {
+			"Authorization": "Bearer " + accessToken,
+			"ocp-apim-subscription-key": subscriptionKey
+		},
+		data: JSON.stringify({ q: text, lang: "en" }),
+		success: function(data) {
+			prepareResponse(data);
+			setResponse(data.result.speech);
+
+		},
+		error: function() {
+			setResponse("Internal Server Error");
+		}
+	});
+	setResponse("Loading...");
+}
+
+function setResponse(val) {
+	$("#response").text(val);
+}
+
+function prepareResponse(val) {
+  var debugJSON = JSON.stringify(val, undefined, 2),
+      spokenResponse = val.result.speech;
+
+  respond(spokenResponse);
+  // debugRespond(debugJSON);
+}
+
+function respond(val) {
+  if (val == "") {
+    val = messageSorry;
+  }
+
+  if (val !== messageRecording) {
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    msg.voiceURI = "native";
+    msg.text = val;
+    msg.lang = "en-US";
+      window.speechSynthesis.speak(msg);
+
+    msg.onend = function (event) { 
+    	switchRecognition();
+		};
+      // while (window.speechSynthesis.speaking){
+
+      // }
+      // switchRecognition();
+  }
+
+  $("#spokenResponse").addClass("is-active").find(".spoken-response__text").html(val);
+}
