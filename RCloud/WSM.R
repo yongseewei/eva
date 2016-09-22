59aaa94530bca5af9d650c44ed77f185fe912fe6
Jenping C <jenping.chong@gmail.com>
2016-09-23 00:54:48 +0800

    Create WSM.R
    Decision Making Module

---
+++ RCloud/WSM.R
@@ -0,0 +1,52 @@
+# keep snippets here while working with your notebook's cells
+calculateWSM <- function(dataTable) {
+    # Normalize the numerical data
+    rating <- normalizeRating(dataTable["Rating"])
+    price <- linearNormalize(dataTable["Price"])
+    distance <- linearNormalize(dataTable["Distance"])
+    
+    # create a new data frame/table
+    normTable <- data.frame(rating, price, distance)
+    normTableColNum <- length(normTable)
+    normTableRowNum <- nrow(normTable)
+    
+    # Get ROC weights based on number of columns/factors
+    ROC <- getROC(normTableColNum)
+    
+    # Calculate the WSM score for each row
+    score <- data.frame()
+    for(i in 1:normTableRowNum) {
+        row <- normTable[i,]
+        score[i, 1] <- ROC[1]*row["Rating"] + ROC[2]*(1-row["Price"]) + ROC[3]*(1-row["Distance"])
+    }
+    
+    # Append score to table, order preserved
+    dataTable["Score"] <- score
+    
+    # Sort the table by score, descending order
+    dataTable <- dataTable[with(dataTable, order(-Score)), ]
+    
+    return(dataTable)
+}
+
+
+getROC <- function(numOfRanks) {
+    ROCList <- list()
+    for (i in 1:numOfRanks) {
+        weight <- 0
+        for (j in i:numOfRanks) {
+            weight <- weight + (1/j) 
+        }
+        weight <- weight / numOfRanks
+        ROCList[i] <- weight
+    }
+    return(ROCList)
+}
+
+linearNormalize <- function(x) {
+   (x-min(x))/(max(x)-min(x))
+}
+
+normalizeRating <- function(x) {
+   (x-1)/(5-1)
+}
